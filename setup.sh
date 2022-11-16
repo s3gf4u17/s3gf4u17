0f27da294d20491928ec68be856d02267b717bb9
szkolakowski <szymon.kolakowski@gmail.com>
2022-11-16 02:01:10 +0000

    update setup

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 sudo apt-get update && sudo apt-get upgrade -y
 
 sudo apt install virtualbox -y
+sudo apt-get install linux-headers-`uname -r`
+sudo dpkg-reconfigure virtualbox-dkms
+sudo modprobe vboxdrv
